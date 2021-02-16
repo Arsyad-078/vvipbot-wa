b289d70abe7ee58f77676a64740ccccf266c784c
Arsyad-078 <75372519+Arsyad-078@users.noreply.github.com>
2021-02-16 15:28:42 +0800

    Update index.js

--- index.js
+++ index.js
@@ -16,13 +16,13 @@ const menu = require("./lib/menu.js")
 const apivhtear = 'Apikey vhtear';
 const apibarbar = 'Apikey mhankbarbar';
 const BotName = 'VVIPINDO BOT'; 
-const instagram = 'agus_alnsyh71- kaelMk_'; 
+const instagram = 'muhammad_rasyad.13'; 
 const aktif = 'Kapan bot aktif';
 const vcard = 'BEGIN:VCARD\n' // metadata of the contact card
             + 'VERSION:3.0\n' 
-            + 'FN:Agus\n' // Nama kamu
-            + 'ORG:VVIPINDO;\n' // Nama bot
-            + 'TEL;type=CELL;type=VOICE;waid=6289613469459:+62 896-1346-9459\n' //Nomor whatsapp kamu
+            + 'FN:Arsyad ACT\n' // Nama kamu
+            + 'ORG:BoT-DrOiD;\n' // Nama bot
+            + 'TEL;type=CELL;type=VOICE;waid=6283141973410:+62 831-4197-3410\n' //Nomor whatsapp kamu
             + 'END:VCARD'
 const
 {
